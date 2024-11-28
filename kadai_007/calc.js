0bc1c4a7d29fadabf4696f737494d1921fd2b42f
kamishosuzu1220 <lessonpiyo@gmail.com>
2024-11-28 23:27:50 +0900

    課題007の再提出

--- kadai_007/calc.js
+++ kadai_007/calc.js
@@ -1,17 +1,17 @@
-// 変数numに0〜15までのランダムな整数を代入する
-let num = Math.floor(Math.random() * 15 + 1);
+// 変数numに1〜60までのランダムな整数を代入する
+let num = Math.floor(Math.random() * 60 + 1);
 
 // 変数numの値を出力する（確認用）
 console.log(num);
 
 // 変数numの値が3の倍数であれば、「3の倍数です」という文字列を表示させる
-if(num === 15) {
+if(num % 3 === 0 && num % 5 === 0) {
     console.log('3と5の倍数です');
 }    
-else if(num === 3 || num === 6 || num === 9 || num === 12) {
+else if(num % 3 === 0) {
     console.log('3の倍数です');
 }
-else if(num === 5 || num === 10) {
+else if(num % 5 === 0) {
     console.log('5の倍数です');
 }
 else {
